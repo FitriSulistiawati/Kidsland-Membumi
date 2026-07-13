6bd734047ab525f4cd807e861bae23ecb0ee57bb
atsalhfdz5 <matsal.hafidz_ti22@nusaputra.ac.id>
2026-07-13 23:53:00 +0700

    Fix login API URL

--- kidsland-react/src/pages/Login.tsx
+++ kidsland-react/src/pages/Login.tsx
@@ -10,7 +10,7 @@ export default function Login() {
     e.preventDefault();
     
     try {
-      const response = await fetch("http://127.0.0.1:5000/login", {
+      const response = await fetch("https://kidsland-membumi-production.up.railway.app/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
